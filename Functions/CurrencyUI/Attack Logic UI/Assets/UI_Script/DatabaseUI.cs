d3026cc0db19c39e83febd4d05cc31d701f47934
geunwhom <geunwook57@gmail.com>
2025-10-22 22:08:58 -0500

    Debug UI update
    Enemy&Team currently spawned checking

--- Functions/CurrencyUI/Attack Logic UI/Assets/UI_Script/DatabaseUI.cs
+++ Functions/CurrencyUI/Attack Logic UI/Assets/UI_Script/DatabaseUI.cs
@@ -52,60 +52,58 @@ public class DatabaseUI : MonoBehaviour
     }
 
     private void RefreshData()
-{
-    var enemies = EntityManager.getEnemyList();
-    var teams = EntityManager.getTeamList();
-    var deadEnemies = EntityManager.getDeadEnemyList();
-    var deadTeams = EntityManager.getDeadTeamList();
-
-    // Group by type
-    var enemyTypeCounts = enemies
-        .GroupBy(e => e.name.Replace("(Clone)", ""))
-        .OrderBy(g => g.Key) // ensure fixed order
-        .ToDictionary(g => g.Key, g => g.Count());
-
-    var deadEnemyTypeCounts = deadEnemies
-        .GroupBy(n => n)
-        .OrderBy(g => g.Key)
-        .ToDictionary(g => g.Key, g => g.Count());
-
-    int totalEnemiesAlive = enemies.Count;
-    int totalEnemiesDead = deadEnemies.Count;
-    int totalTeamsAlive = teams.Count;
-    int totalTeamsDead = deadTeams.Count;
-    int totalEnemiesSpawned = totalEnemiesAlive + totalEnemiesDead;
-    int totalTeamsSpawned = totalTeamsAlive + totalTeamsDead;
-
-    StringBuilder sb = new StringBuilder();
-
-    sb.AppendLine("<b>DATABASE</b>");
-    sb.AppendLine();
-    sb.AppendLine($"<b>Total Enemies:</b> {totalEnemiesAlive} alive / {totalEnemiesDead} dead (Spawned {totalEnemiesSpawned})");
-    sb.AppendLine($"<b>Total Teams:</b> {totalTeamsAlive} alive / {totalTeamsDead} dead (Spawned {totalTeamsSpawned})");
-    sb.AppendLine();
-    sb.AppendLine("<b>Enemy Types:</b>");
-    sb.AppendLine("-------------------------");
-
-    if (enemyTypeCounts.Count == 0 && deadEnemyTypeCounts.Count == 0)
     {
-        sb.AppendLine("None");
-    }
-    else
-    {
-        // merge both dictionaries to keep all known enemy types
-        var allTypes = enemyTypeCounts.Keys.Union(deadEnemyTypeCounts.Keys).OrderBy(k => k);
-        foreach (var typeName in allTypes)
+        var enemies = EntityManager.getEnemyList();
+        var teams = EntityManager.getTeamList();
+        var deadEnemies = EntityManager.getDeadEnemyList();
+        var deadTeams = EntityManager.getDeadTeamList();
+
+        var enemyTypeCounts = enemies
+            .GroupBy(e => e.name.Replace("(Clone)", ""))
+            .OrderBy(g => g.Key) 
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        var deadEnemyTypeCounts = deadEnemies
+            .GroupBy(n => n)
+            .OrderBy(g => g.Key)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        int totalEnemiesAlive = enemies.Count;
+        int totalEnemiesDead = deadEnemies.Count;
+        int totalTeamsAlive = teams.Count;
+        int totalTeamsDead = deadTeams.Count;
+        int totalEnemiesSpawned = totalEnemiesAlive + totalEnemiesDead;
+        int totalTeamsSpawned = totalTeamsAlive + totalTeamsDead;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("<b>DATABASE</b>");
+        sb.AppendLine();
+        sb.AppendLine($"<b>Total Enemies:</b> {totalEnemiesAlive} alive / {totalEnemiesDead} dead (Spawned {totalEnemiesSpawned})");
+        sb.AppendLine($"<b>Total Teams:</b> {totalTeamsAlive} alive / {totalTeamsDead} dead (Spawned {totalTeamsSpawned})");
+        sb.AppendLine();
+        sb.AppendLine("<b>Enemy Types:</b>");
+        sb.AppendLine("-------------------------");
+
+        if (enemyTypeCounts.Count == 0 && deadEnemyTypeCounts.Count == 0)
         {
-            int alive = enemyTypeCounts.ContainsKey(typeName) ? enemyTypeCounts[typeName] : 0;
-            int dead = deadEnemyTypeCounts.ContainsKey(typeName) ? deadEnemyTypeCounts[typeName] : 0;
-            int total = alive + dead;
-            sb.AppendLine($"  {typeName}: {alive} alive / {dead} dead (Total {total})");
+            sb.AppendLine("None");
         }
+        else
+        {
+            var allTypes = enemyTypeCounts.Keys.Union(deadEnemyTypeCounts.Keys).OrderBy(k => k);
+            foreach (var typeName in allTypes)
+            {
+                int alive = enemyTypeCounts.ContainsKey(typeName) ? enemyTypeCounts[typeName] : 0;
+                int dead = deadEnemyTypeCounts.ContainsKey(typeName) ? deadEnemyTypeCounts[typeName] : 0;
+                int total = alive + dead;
+                sb.AppendLine($"  {typeName}: {alive} alive / {dead} dead (Total {total})");
+            }
+        }
+
+        sb.AppendLine();
+
+        summaryText.text = sb.ToString();
     }
-
-    sb.AppendLine();
-
-    summaryText.text = sb.ToString();
-}
 
 }
