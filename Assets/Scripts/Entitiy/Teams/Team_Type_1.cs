d08adca8a7ea7cfd63307628dd2dc4445213949b
twoSquaredHoon <twosquaredhoon@gmail.com>
2025-10-24 13:46:01 -0500

    Team_Type_1 animation applied

--- Assets/Scripts/Entitiy/Teams/Team_Type_1.cs
+++ Assets/Scripts/Entitiy/Teams/Team_Type_1.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Team_Type_1 : Team
 {
+    public Animator anim;
     protected override void Start()
     {
         base.Start();
         hp = 50f;
         attackPower = 8f;
         attackSpeed = 0.41f;
-        moveSpeed = 0.5f;
+        moveSpeed = 5f;
+    }
+    void LateUpdate()
+    {
+        anim.SetBool("isMoving", canMove);
     }
 }
